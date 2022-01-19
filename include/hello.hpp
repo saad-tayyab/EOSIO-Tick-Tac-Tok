230ef55548ccd02b7191616ef67254380a46fd37
Saad Tayyab <saad_tayyab@outlook.com>
2022-01-19 17:43:47 +0500

    tic tac toe game implemented

--- include/hello.hpp
+++ include/hello.hpp
@@ -4,34 +4,82 @@
 using namespace eosio;
 using namespace std;
 
-CONTRACT hello : public contract
-{
+// The contract
+CONTRACT hello : public contract {
 public:
-    using contract::contract;
-    hello(name receiver, name code, datastream<const char *> ds) : contract(receiver, code, ds) {}
 
-
-    struct [[eosio::table]] game
-    {
+TABLE game_table {
+        uint64_t id;
+        vector<std::uint8_t> board{0,0,0,0,0,0,0,0,0};
         name challenger;
         name host;
+        name turn;
+        name winner;
 
-        auto primary_key() const { return challenger.value; }
-        uint128_t secondary_key() const {return challenger.value;}
-        EOSLIB_SERIALIZE( game, (challenger)(host))
-    };
+        bool is_empty_cell(uint16_t row, uint16_t col) {return (board[row*3+col]==0);}
+        bool is_valid_movement(name by, uint16_t row, uint16_t col) {
+            if (winner==name() && by==turn && is_empty_cell(row, col) ) {
+                uint8_t set=1;
+                if (by==host) {
+                    board[row*3+col]=set;
+                    turn=challenger;
+                }
+                else {
+                    set=2;
+                    board[row*3+col]=set;
+                    turn=host;
+                }
 
-//    typedef eosio::multi_index<"games"_n, game> games;
+                uint8_t test=((board[row*3+0]&board[row*3+1]&board[row*3+2])
+                              |(board[0*3+col]&board[1*3+col]&board[2*3+col])
+                              |(board[0]&board[4]&board[8])
+                              |(board[2]&board[4]&board[6]));
 
-    typedef eosio::multi_index<"games"_n, game,
-            eosio::indexed_by<"gameskey"_n, eosio::const_mem_fun<game, uint128_t, &game::secondary_key>>
-    > game_index;
+                if (test==set)
+                    winner=by;
 
+                return true;
+            }
+            return false;
+        }
 
-    [[eosio::action]]
-    void create(const name &challenger, name &host);
+        uint64_t primary_key() const {return id;}
+        uint128_t secondary_key() const {return combine_ids(host.value,challenger.value);}
+        EOSLIB_SERIALIZE(game_table, (id)(board)(challenger)(host)(turn)(winner))
+};
 
-    [[eosio::action]]
-    void close(const name &challenger, const name &host);
+typedef eosio::multi_index<name("game"), game_table,
+        eosio::indexed_by<name("gameskey"), eosio::const_mem_fun<game_table, uint128_t, &game_table::secondary_key>>
+> game_index;
 
+TABLE leader_board {
+        name winner;
+        uint64_t count;
+        uint64_t primary_key() const {return winner.value;}
+        uint64_t secondary_key() const {return -count;}
+        EOSLIB_SERIALIZE(leader_board, (winner)(count))
+};
+
+typedef eosio::multi_index<name("leader"), leader_board,
+        eosio::indexed_by<name("leaderskey"), eosio::const_mem_fun<leader_board, uint64_t, &leader_board::secondary_key>>
+> leader_index;
+
+// Use contract's constructor
+using contract::contract;
+
+ACTION welcome(name host, name opponent) ;
+
+ACTION create(name challenger, name host) ;
+
+ACTION restart(name challenger, name host, name by) ;
+
+ACTION move(name challenger, name host, name by, uint16_t row, uint16_t col) ;
+
+ACTION close(name challenger, name host) ;
+
+private:
+// concatenation of ids example
+static uint128_t combine_ids(const uint64_t &x, const uint64_t &y) {
+    return (uint128_t{x} << 64) | y;
+}
 };
--- src/hello.cpp
+++ src/hello.cpp
@@ -1,39 +1,82 @@
 #include <hello.hpp>
-using namespace eosio;
 
 
-void hello::create(const name &challenger, name &host) {
+ACTION hello::welcome(name host, name opponent) {
+    //action can only be taken by contract account
+    require_auth(get_self());
+    print("Welcome, ", host, " and ", opponent,"!");
+}
+
+ACTION hello::create(name challenger, name host) {
     require_auth(host);
-    check(challenger != host, "failed, challenger shouldn't be the same as host");
+    check(host!=challenger, "Could not challenge youself!");
+    //check(is_account(challenger),"challenger account not found");
 
-    // Check if game already exists
-    game_index existing_host_games(get_self(), host.value);
+    game_index _game(get_self(), get_self().value);
+    //find using secondary key
+    auto _gameskey = _game.get_index<name("gameskey")>(); //set secondary key
+    auto itrh = _gameskey.find(combine_ids(host.value, challenger.value));
+    auto itrc = _gameskey.find(combine_ids(challenger.value, host.value));
+    check(itrh==_gameskey.end() && itrc==_gameskey.end(), "game already exists!"); //if found
 
-    auto _gameskey = existing_host_games.get_index<name("gameskey")>(); //set secondary key
-
-    auto itrc = _gameskey.find(challenger.value);
-    check(itrc==_gameskey.end(), "game already exists!sa"); //if found
-
-    auto itr = existing_host_games.find(challenger.value);
-    check(itr == existing_host_games.end(), "game already exists");
-
-    existing_host_games.emplace(host, [&](auto &g) {
-        g.challenger = challenger;
-        g.host = host;
+    //ram charge to action caller
+    _game.emplace(get_first_receiver(), [&](auto& game) {
+        game.host = host;
+        game.challenger = challenger;
+        game.turn = host;
     });
 }
 
-void hello::close(const name &challenger, const name &host)
-{
-    check(has_auth(host), "only the host can close the game");
+ACTION hello::restart(name challenger, name host, name by) {
+    check(has_auth(by), "please auth before!");
+    check(by==challenger || by==host, "only challenger or host can restart game!");
+    game_index _game(get_self(), get_self().value);
+    auto _gameskey = _game.get_index<name("gameskey")>();
+    auto itr = _gameskey.find(combine_ids(host.value, challenger.value));
+    check(itr!=_gameskey.end(),"create game first!");
+    _game.modify(*itr, same_payer, [&]( auto& game ) {
+        fill(game.board.begin(), game.board.end(), 0);
+        game.turn=by;
+        game.winner=name();
+    });
+}
 
-    require_auth(host);
+ACTION hello::move(name challenger, name host, name by, uint16_t row, uint16_t col) {
+    check(has_auth(by), "please auth before!");
+    check(by==challenger || by==host, "only challenger or host can move!");
+    check(row<3 && col < 3, "invalid row or col!");
+    game_index _game(get_self(), get_self().value);
+    auto _gameskey = _game.get_index<
+            name("gameskey")>();
+    auto itr = _gameskey.find(combine_ids(host.value, challenger.value));
+    check(itr!=_gameskey.end(), "create game first!");
+    check((*itr).winner==name(), "game over!");
+    _game.modify(*itr, same_payer, [&]( auto& game ) {
+        check(game.is_valid_movement(by, row, col), "invalid movement!");
+    });
+    if ((*itr).winner!=name()) {
+        leader_index _leader(get_self(), get_self().value);
+        auto itrl = _leader.find((*itr).winner.value);
+        if (itrl == _leader.end()) {
+            _leader.emplace(get_first_receiver(), [&](auto& leader) {
+                leader.winner=(*itr).winner;
+                leader.count=1;
+            });
+        }
+        else {
+            _leader.modify(*itrl, same_payer, [&]( auto& leader) {
+                leader.count++;
+            });
+        }
+    }
+}
 
-    // Check if game exists
-    game_index existing_host_games(get_self(), host.value);
-    auto itr = existing_host_games.find(challenger.value);
-    check(itr != existing_host_games.end(), "game doesn't exists");
-
-    // Remove game
-    existing_host_games.erase(itr);
+ACTION hello::close(name challenger, name host) {
+    //find by host (primary key)
+    check(has_auth(host) || has_auth(challenger), "please auth yourself!");
+    game_index _game(get_self(), get_self().value);
+    auto _gameskey = _game.get_index<name("gameskey")>();
+    auto itr = _gameskey.find(combine_ids(host.value, challenger.value));
+    check(itr!=_gameskey.end(), "game not found!");
+    _gameskey.erase(itr);
 }
